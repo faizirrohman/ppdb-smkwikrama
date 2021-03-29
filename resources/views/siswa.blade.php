e4ec1e6e3d46c9ac6008bdb87327afb6c08b8896
faizirrohman8@gmail.com <faizirrohman8@gmail.com>
2021-03-29 16:28:09 +0700

    Initial commit

---
+++ resources/views/siswa.blade.php
@@ -0,0 +1,153 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>PPDB SMK Wikrama Bogor</title>
+    <link rel="icon" href="img/favicon.png">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="{{ asset ('css/bootstrap.min.css') }}">
+    <!-- animate CSS -->
+    <link rel="stylesheet" href="{{ asset('css/animate.css') }}">
+    <!-- owl carousel CSS -->
+    <link rel="stylesheet" href="{{ asset('css/owl.carousel.min.css') }}">
+    <!-- themify CSS -->
+    <link rel="stylesheet" href="{{ asset('css/themify-icons.css') }}">
+    <!-- flaticon CSS -->
+    <link rel="stylesheet" href="{{ asset('css/flaticon.css') }}">
+    <!-- font awesome CSS -->
+    <link rel="stylesheet" href="{{ asset('css/magnific-popup.css') }}">
+    <!-- swiper CSS -->
+    <link rel="stylesheet" href="{{ asset('css/slick.css') }}">
+    <!-- style CSS -->
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+</head>
+
+<body>
+    <!--::header part start::-->
+    <header class="main_menu home_menu">
+        <div class="container">
+            <div class="row align-items-center">
+                <div class="col-lg-12">
+                    <nav class="navbar navbar-expand-lg navbar-light">
+                        <a class="navbar-brand" href="index.html"><h2>PPDB SMK WIKRAMA BOGOR</h2></a>    
+                        <button class="navbar-toggler" type="button" data-toggle="collapse"
+                            data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                            aria-expanded="false" aria-label="Toggle navigation">
+                            <span class="navbar-toggler-icon"></span>
+                        </button>
+
+                        <div class="collapse navbar-collapse main-menu-item justify-content-end"
+                            id="navbarSupportedContent">
+                            <ul class="navbar-nav align-items-center">
+                                <li class="nav-item">
+                                    <a class="nav-link" href="/siswa/daftar">DAFTAR SEKARANG</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </nav>
+                </div>
+            </div>
+        </div>
+    </header>
+    <!-- Header part end-->
+
+    <!-- banner part start-->
+    <section class="banner_part">
+        <div class="container">
+            <div class="row align-items-center">
+                <div class="col-lg-6 col-xl-6">
+                    <div class="banner_text">
+                        <div class="banner_text_iner">
+                              <h5>SELAMAT DATANG</h5>
+                              <br>
+                              <h2>PPDB SMK WIKRAMA BOGOR</h2>
+                              <br>
+                              <p>Ilmu yang amaliah, amal yang ilmiah, akhlakul karimah</p>
+                              <a href="/siswa/daftar" class="btn_1">Daftar Sekarang</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- banner part start-->
+
+    <!-- footer part start-->
+    <footer class="footer-area">
+        <div class="container">
+            <div class="row justify-content-between">
+                  <div class="col-sm-6 col-md-4 col-xl-4">
+                        <div class="single-footer-widget footer_2">
+                              <h4>SMK WIKRAMA BOGOR</h4>
+                              <p>Ilmu yang amaliah, amal yang ilmiah, akhlakul karimah</p>
+                        </div>
+                        <div class="social_icon">
+                              <a href="#"> <i class="ti-facebook"></i> </a>
+                              <a href="#"> <i class="ti-twitter-alt"></i> </a>
+                              <a href="#"> <i class="ti-instagram"></i> </a>
+                              <a href="#"> <i class="ti-skype"></i> </a>
+                        </div>
+                    </div>
+                  <div class="col-sm-6 col-md-4 col-xl-4">
+                        <div class="single-footer-widget footer_2">
+                              <h4>Contact us</h4>
+                              <div class="contact_info">
+                                    <p><span> Address :</span> Jl. Raya Wangun Kel. Sindangsari Kec. Bogor Timur Kota Bogor 16146 </p>
+                                    <p><span> Phone :</span> 0251 8242411</p>
+                                    <p><span> Email : </span>prohumasi@smkwikrama.sch.id </p>
+                                    <p><span> Website : </span><a href="http://smkwikrama.sch.id/" target="_blank">SMK Wikrama Bogor</a> </p>
+                              </div>
+                        </div>
+                  </div>
+                  <div class="col-xl-3 col-sm-6 col-md-4">
+                        <div class="single-footer-widget footer_2">
+                        </div>
+                  </div>
+            </div>
+        </div>
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="copyright_part_text text-center">
+                        <div class="row">
+                            <div class="col-lg-12">
+                                <p class="footer-text m-0"><!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+Copyright &copy;<script>document.write(new Date().getFullYear());</script> PPDB SMK WIKRAMA
+<!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. --></p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </footer>
+    <!-- footer part end-->
+
+    <!-- jquery plugins here-->
+    <!-- jquery -->
+    <script src="{{ asset('js/jquery-1.12.1.min.js') }}"></script>
+    <!-- popper js -->
+    <script src="{{ asset('js/popper.min.js') }}"></script>
+    <!-- bootstrap js -->
+    <script src="{{ asset('js/bootstrap.min.js') }}"></script>
+    <!-- easing js -->
+    <script src="{{ asset('js/jquery.magnific-popup.js') }}"></script>
+    <!-- swiper js -->
+    <script src="{{ asset('js/swiper.min.js') }}"></script>
+    <!-- swiper js -->
+    <script src="{{ asset('js/masonry.pkgd.js') }}"></script>
+    <!-- particles js -->
+    <script src="{{ asset('js/owl.carousel.min.js') }}"></script>
+    <script src="{{ asset('js/jquery.nice-select.min.js') }}"></script>
+    <!-- swiper js -->
+    <script src="{{ asset('js/slick.min.js') }}"></script>
+    <script src="{{ asset('js/jquery.counterup.min.js') }}"></script>
+    <script src="{{ asset('js/waypoints.min.js') }}"></script>
+    <!-- custom js -->
+    <script src="{{ asset('js/custom.js') }}"></script>
+</body>
+
+</html>
